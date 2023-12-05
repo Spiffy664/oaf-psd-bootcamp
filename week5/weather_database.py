46f8f45fc40c9707d57c09f81c522eabcf61631d
Spiffy664 <spacemanspiff982@gmail.com>
2023-12-05 03:21:16 -0500

    Create weather_database.py
    new weather_database python file

---
+++ week5/weather_database.py
@@ -0,0 +1,67 @@
+import sqlite3
+from datetime import datetime, timedelta
+
+class WeatherDatabase:
+
+    def __init__(self, db_file):
+
+        self.conn = sqlite3.connect(":memory:")
+
+        self.cursor = self.conn.cursor()
+
+
+        self.cursor.execute('''
+            CREATE TABLE IF NOT EXISTS weather(
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                city TEXT,
+                temperature REAL,
+                time TEXT
+            )
+        ''')
+
+    def insert_data(self, city, temperature, time):
+
+        timestamp = datetime.now()
+
+        self.cursor.execute('''
+            SELECT * FROM weather
+            WHERE city = ?
+            ORDER BY time DESC
+            LIMIT 1
+        ''', (city, ))
+        
+        data = self.cursor.fetchone()
+        
+        # Check if data exists and if it is older than 24 hours before inserting
+        if data and datetime.strptime(data[4], "%Y-%m-%d %H:%M:%S") > timestamp - timedelta(hours=24):
+            return data
+        else:
+            self.cursor.execute('''
+            INSERT INTO weather (city, temperature, time, timestamp) VALUES (?, ?, ?, ?)
+            ''', (city, temperature, time, timestamp.strftime("%Y-%m-%d %H:%M:%S")))
+            self.conn.commit()
+
+    def get_weather_data(self, weather_service, city):
+
+        self.cursor.excute('''
+            SELECT * FROM weather
+            WHERE city = ?
+        ''', (city,))
+        data = self.cursor.fetchall()
+
+        if data:
+            return data
+        else:
+            weather_data = weather_service.get_weather_data(city)
+
+            self.cursor.excute('''
+                INSERT INTO weather (city, temperature)
+                VALIES (?, ?)
+            ''', (city, str(weather_data)))
+            self.conn.commit()
+
+            return weather_data
+        
+    def close_connect(self):
+        self.conn.close()
+        print("Database connection closed")
